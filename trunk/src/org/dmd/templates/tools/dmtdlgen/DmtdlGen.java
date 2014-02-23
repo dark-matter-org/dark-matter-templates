332c6b0d56876ece1f674fddcd7b6a4736d1594d
pstrong99@gmail.com <pstrong99@gmail.com@396c5938-4266-316f-cf90-5f785b06447c>
2014-02-23 18:22:06 +0000

    Further work on the extension hook

--- trunk/src/org/dmd/templates/tools/dmtdlgen/DmtdlGen.java
+++ trunk/src/org/dmd/templates/tools/dmtdlgen/DmtdlGen.java
@@ -4,11 +4,15 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
 
+import org.dmd.templates.server.extended.ContainedElement;
+import org.dmd.templates.server.extended.ExtensionHook;
 import org.dmd.templates.server.extended.Section;
 import org.dmd.templates.server.extended.TdlModule;
 import org.dmd.templates.server.extended.TextualArtifact;
+import org.dmd.templates.server.generated.dmw.ContainsIterableDMW;
 import org.dmd.templates.server.generated.dsd.TdlModuleDefinitionManager;
 import org.dmd.templates.server.generated.dsd.TdlModuleGenUtility;
+import org.dmd.templates.shared.generated.types.Contains;
 import org.dmd.util.FileUpdateManager;
 import org.dmd.util.exceptions.ResultException;
 import org.dmd.util.parsing.ConfigLocation;
@@ -17,8 +21,36 @@ public class DmtdlGen extends TdlModuleGenUtility {
 
 	@Override
 	public void parsingComplete(TdlModule module, ConfigLocation location, TdlModuleDefinitionManager definitions) throws ResultException {
-		// TODO Auto-generated method stub
+		onlySectionsInArtifact(module);
+	}
+	
+	/**
+	 * Checks that TextualArtifacts only refer to Sections, not ExtensionHooks.
+	 * @param module
+	 * @throws ResultException
+	 */
+	void onlySectionsInArtifact(TdlModule module) throws ResultException{
+		ResultException ex = null;
 
+		Iterator<TextualArtifact> artifacts = module.getAllTextualArtifact();
+		while(artifacts.hasNext()){
+			TextualArtifact artifact = artifacts.next();
+			ContainsIterableDMW it = artifact.getContainsIterable();
+			while(it.hasNext()){
+    			Contains c = it.getNext();
+    			ContainedElement ce = (ContainedElement) c.getElement().getObject().getContainer();
+    			
+    			if (ce instanceof ExtensionHook){
+    				if (ex == null)
+    					ex = new ResultException();
+    				ex.addError("ExtensionHook: " + ce.getName() + " can only be used in Sections, not TextualArtifacts");
+    			}
+			}
+			if (ex != null){
+				ex.setLocationInfo(artifact.getFile(), artifact.getLineNumber());
+				throw(ex);
+			}
+		}
 	}
 
 	@Override
